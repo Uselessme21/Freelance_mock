4206f61d6acd727f294e8b27daa7b3cf6b5e6171
rajneesh <mr.rajneeshy@gmail.com>
2023-11-23 21:54:29 +0530

    done

--- jsonServer/index.js
+++ jsonServer/index.js
@@ -1,10 +1,11 @@
 const jsonServer = require("json-server"); 
 const server = jsonServer.create();
+const cors = require('cors');
 require("dotenv").config();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
-
+server.use(cors())
 
 server.use( middlewares);
 server.use( router);
