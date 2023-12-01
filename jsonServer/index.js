9f782de571011cebe0a5538599751e0f11ef614c
Rajneesh Yadav <112797112+Uselessme21@users.noreply.github.com>
2023-12-01 23:30:04 +0530

    Update index.js

--- jsonServer/index.js
+++ jsonServer/index.js
@@ -1,15 +1,10 @@
-const jsonServer = require("json-server"); 
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const cors = require('cors');
-require("dotenv").config();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
-server.use(cors())
+const port = process.env.PORT || 8080; 
 
-server.use( middlewares);
-server.use( router);
+server.use(middlewares);
+server.use(router);
 
-server.listen(port,()=>{
-    console.log("listening on port", port)
-});
+server.listen(port);
